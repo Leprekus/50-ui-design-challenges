15e188557514fda6012e7695964078a6bcf5ace7
Leprekus <leprekus@yahoo.com>
2022-05-10 19:39:58 -0700

    open & close checkout menu

--- credit-card-checkout/main.js
+++ credit-card-checkout/main.js
@@ -1,12 +1,21 @@
 const closeButton = document.getElementById('close-button')
+const transitionWrapper = document.getElementById('transition-wrapper')
+
+const openMenu = document.getElementById('open-menu')
+const orderSummary = document.getElementById('order-summary')
 
 function toggleDisplay(domElement) {
-    if(closeButton.style.display === 'none') {
-        closeButton.style.display === 'block'
+    if(domElement.style.display !== 'block') {
+        domElement.style.display = 'block'
+    } else {
+        domElement.style.display = 'none'
     }
-
-    closeButton.style.display === 'none'
-
 }
 
-closeButton.addEventListener('click', toggleDisplay)
+openMenu.addEventListener('click', ()=> toggleDisplay(transitionWrapper))
+
+closeButton.addEventListener('click', () => {
+    toggleDisplay(closeButton)
+    toggleDisplay(transitionWrapper)
+})
+
